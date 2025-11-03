9fab0822a5d128bf0b948c374836c7ae21622083
rajeshpurella <rajeshpurella2002@gmail.com>
2025-11-03 23:26:02 +0530

    firstcommit

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ export default function App() {
       // setNasaRaw(nasaRawResponse);
     } catch (err) {
       console.error("LoadLive error:", err);
-      alert("https://solar-backend-0wcy.onrender.com");
+      
     } finally {
       setLoading(false);
     }
